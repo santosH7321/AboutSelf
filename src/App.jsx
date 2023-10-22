b0a1ae8a740df1e63d1efb913106a74e9030e5c6
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-10-22 17:10:15 +0530

    AboutSelf

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from './Components/Nav';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,23 @@
+
+function Nav(){
+    <div>
+        <div>
+            <div>
+                <p>Welcome</p>
+            </div>
+        
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Project</li>
+                    <li>Achivment</li>
+                    <li>Contact</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+}
+
+export default Nav;
